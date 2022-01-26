5a4db8cb00b93b244dc849869d997763a1fae6b9
Nemanja <manojlovic911@hotmail.com>
2022-01-26 09:51:43 +0100

    Add Layout comp to the app.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AllDetail from "./pages/AllDetail";
 import NewQuote from "./pages/NewQuote";
 import AllQuotes from "./pages/AllQuotes";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Routes>
         <Route path="/" element={<Navigate replace to="/quotes" />} />
         <Route path={"/quotes"} element={<AllQuotes />} />
         <Route path={"/quotes/:quoteId/*"} element={<AllDetail />} />
         <Route path={"/new-quote"} element={<NewQuote />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
