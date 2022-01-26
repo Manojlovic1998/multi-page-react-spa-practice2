664faaeeb9ec2ac187a6c2945b1fdf9cccc94827
Nemanja <manojlovic911@hotmail.com>
2022-01-26 16:20:41 +0100

    Add HighlightedQuote comp to QuoteDetail page and cond rendering of page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,24 @@
 import { Fragment } from "react";
 import { useParams, Route, Routes } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
-const AllDetail = () => {
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning react is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+];
+
+const QuoteDetail = () => {
   const params = useParams();
-  console.log(params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>All Detail Page</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Routes>
         <Route path={"/comments"} element={<Comments />} />
       </Routes>
@@ -15,4 +26,4 @@ const AllDetail = () => {
   );
 };
 
-export default AllDetail;
+export default QuoteDetail;
