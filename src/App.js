4278c14c66cff980534ef3bca7ff6343bd11b1e6
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-01-26 07:02:42 +0100

    Hook navigation with pages using react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AllDetail from './pages/AllDetail';
+import NewQuote from './pages/NewQuote';
+import AllQuotes from './pages/AllQuotes';
+
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/quotes"/>}/>
+        <Route path={"/quotes"} element={ <AllQuotes /> }/>
+        <Route path={"/quotes/:quoteId"} element={<AllDetail />}/>
+        <Route path={"/new-quote"} element={<NewQuote />}/>
+      </Routes>
     </div>
   );
 }
