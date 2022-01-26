a8799c08fd4ab30855b006ec2de7d45d86fce635
Nemanja <manojlovic911@hotmail.com>
2022-01-26 16:06:59 +0100

    Update name of QuoteDetail page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import AllDetail from "./pages/AllDetail";
+import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
 import AllQuotes from "./pages/AllQuotes";
 import Layout from "./components/layout/Layout";
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate replace to="/quotes" />} />
         <Route path={"/quotes"} element={<AllQuotes />} />
-        <Route path={"/quotes/:quoteId/*"} element={<AllDetail />} />
+        <Route path={"/quotes/:quoteId/*"} element={<QuoteDetail />} />
         <Route path={"/new-quote"} element={<NewQuote />} />
       </Routes>
     </Layout>
