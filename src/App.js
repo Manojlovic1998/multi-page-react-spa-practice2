736bfc76bbef8ab2bad0da4f5506e2894adfe6dd
Nemanja <manojlovic911@hotmail.com>
2022-01-26 09:18:36 +0100

    Add nested route for comments component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import AllDetail from './pages/AllDetail';
-import NewQuote from './pages/NewQuote';
-import AllQuotes from './pages/AllQuotes';
-
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import AllDetail from "./pages/AllDetail";
+import NewQuote from "./pages/NewQuote";
+import AllQuotes from "./pages/AllQuotes";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes"/>}/>
-        <Route path={"/quotes"} element={ <AllQuotes /> }/>
-        <Route path={"/quotes/:quoteId"} element={<AllDetail />}/>
-        <Route path={"/new-quote"} element={<NewQuote />}/>
+        <Route path="/" element={<Navigate replace to="/quotes" />} />
+        <Route path={"/quotes"} element={<AllQuotes />} />
+        <Route path={"/quotes/:quoteId/*"} element={<AllDetail />} />
+        <Route path={"/new-quote"} element={<NewQuote />} />
       </Routes>
     </div>
   );
--- src/pages/AllDetail.js
+++ src/pages/AllDetail.js
@@ -1,15 +1,18 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route, Routes } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const AllDetail = () => {
-    const params = useParams();
-    console.log(params.quoteId);
-    return (
-        <Fragment>
-            <h1>All Detail Page</h1>
-            <p>{params.quoteId}</p>
-        </Fragment>
-        )
+  const params = useParams();
+  console.log(params.quoteId);
+  return (
+    <Fragment>
+      <h1>All Detail Page</h1>
+      <Routes>
+        <Route path={"/comments"} element={<Comments />} />
+      </Routes>
+    </Fragment>
+  );
 };
 
 export default AllDetail;
